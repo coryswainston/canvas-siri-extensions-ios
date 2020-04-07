7bdae30d601d27455bfcc2c90c661dfdcf0c6f47
Cory Swainston <swainston.cory89@gmail.com>
2020-04-06 19:22:02 -0600

    Initial Commit

---
+++ CanvasShortcuts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanvasShortcuts
+//
+//  Created by Cory Swainston on 4/6/20.
+//  Copyright © 2020 Cory Swainston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
